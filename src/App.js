f5e552212266d4806a39d9c610cee24b5a3967f6
Ray-linux <ranirava52@gmail.com>
2023-01-24 08:02:54 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import React ,{ useState } from 'react';
 import './App.css';
+import {RiDeleteBack2Fill} from 'react-icons/ri'
+
 
 function App() {
+
+  const [result, setResult] = useState("");
+  const [calc, setCalc] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+
+  const updateCalc = value => {
+    if(
+      ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))
+    )
+    {
+      return;
+    }
+      setCalc(calc + value);
+    if(!ops.includes(value)) {
+      setResult(eval(calc + value).toString())
+    }
+  }
+  
+
+  // const () => updateCalc("/") = (e) => {
+  //   setResult(result.concat(e.target.value))
+  // }
+
+
+  const clearDisplay = () => {
+    setCalc("")
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const deleteLast = () => {
+    if(calc===''){
+      return;
+    }
+    const value = calc.slice(0, -1)
+    setCalc(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div className="calculator">
+    <div className="display">
+    <h2>{calc || 0}</h2>
     </div>
+    <div className="buttons">
+      <input type="button" value="C" className='btn' onClick={clearDisplay}/>
+      <input type="button" value="DEL" className='btn' onClick={deleteLast}/>
+      <input type="button" value="%" className='btn' onClick={() => updateCalc("%")}/>
+      <input type="button" value="/" className='btn' onClick={() => updateCalc("/")}/>
+      <input type="button" value="7" onClick={() => updateCalc("7")}/>
+      <input type="button" value="8" onClick={() => updateCalc("8")}/>
+      <input type="button" value="9" onClick={() => updateCalc("9")}/>
+      <input type="button" value="x" className='btn' onClick={() => updateCalc("*")}/>
+      <input type="button" value="4" onClick={() => updateCalc("4")}/>
+      <input type="button" value="5" onClick={() => updateCalc("5")}/>
+      <input type="button" value="6" onClick={() => updateCalc("6")}/>
+      <input type="button" value="-" className='btn' onClick={() => updateCalc("-")}/>
+      <input type="button" value="1" onClick={() => updateCalc("1")}/>
+      <input type="button" value="2" onClick={() => updateCalc("2")}/>
+      <input type="button" value="3" onClick={() => updateCalc("3")}/>
+      <input type="button" value="+" className='btn' onClick={() => updateCalc("+")}/>
+      <input type="button" value="0" onClick={() => updateCalc("0")}/>
+      <input type="button" value="00" onClick={() => updateCalc("00")}/>
+      <input type="button" value="." onClick={() => updateCalc(".")}/>
+      <input type="button" value="=" className='btn equal' onClick={calculate}/>
+
+    </div>
+   </div>
+   </>
   );
 }
 
