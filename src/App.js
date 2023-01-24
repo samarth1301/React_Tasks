32ce4a9207642dd5304829d9534030ca1ba43f5e
Ray-linux <ranirava52@gmail.com>
2023-01-24 09:13:21 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,82 +1,113 @@
-import React ,{ useState } from 'react';
-import './App.css';
-import {RiDeleteBack2Fill} from 'react-icons/ri'
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [result, setResult] = useState("");
   const [calc, setCalc] = useState("");
 
-  const ops = ['/', '*', '+', '-', '.'];
+  const ops = ["/", "*", "+", "-", ".", "%"];
 
+  const updateCalc = (value) => {
 
-  const updateCalc = value => {
-    if(
-      ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))
-    )
-    {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
       return;
     }
       setCalc(calc + value);
-    if(!ops.includes(value)) {
-      setResult(eval(calc + value).toString())
+      if (!ops.includes(value)) {
+        setResult(eval(calc + value).toString());
     }
-  }
-  
-
-  // const () => updateCalc("/") = (e) => {
-  //   setResult(result.concat(e.target.value))
-  // }
+  };
 
 
   const clearDisplay = () => {
-    setCalc("")
-  }
+    setCalc("");
+  };
 
   const calculate = () => {
     setCalc(eval(calc).toString());
-  }
+  };
 
   const deleteLast = () => {
-    if(calc===''){
+    if (calc === "") {
       return;
     }
-    const value = calc.slice(0, -1)
+    const value = calc.slice(0, -1);
     setCalc(value);
-  }
+  };
 
   return (
-   <>
-   <div className="calculator">
-    <div className="display">
-    <h2>{calc || 0}</h2>
-    </div>
-    <div className="buttons">
-      <input type="button" value="C" className='btn' onClick={clearDisplay}/>
-      <input type="button" value="DEL" className='btn' onClick={deleteLast}/>
-      <input type="button" value="%" className='btn' onClick={() => updateCalc("%")}/>
-      <input type="button" value="/" className='btn' onClick={() => updateCalc("/")}/>
-      <input type="button" value="7" onClick={() => updateCalc("7")}/>
-      <input type="button" value="8" onClick={() => updateCalc("8")}/>
-      <input type="button" value="9" onClick={() => updateCalc("9")}/>
-      <input type="button" value="x" className='btn' onClick={() => updateCalc("*")}/>
-      <input type="button" value="4" onClick={() => updateCalc("4")}/>
-      <input type="button" value="5" onClick={() => updateCalc("5")}/>
-      <input type="button" value="6" onClick={() => updateCalc("6")}/>
-      <input type="button" value="-" className='btn' onClick={() => updateCalc("-")}/>
-      <input type="button" value="1" onClick={() => updateCalc("1")}/>
-      <input type="button" value="2" onClick={() => updateCalc("2")}/>
-      <input type="button" value="3" onClick={() => updateCalc("3")}/>
-      <input type="button" value="+" className='btn' onClick={() => updateCalc("+")}/>
-      <input type="button" value="0" onClick={() => updateCalc("0")}/>
-      <input type="button" value="00" onClick={() => updateCalc("00")}/>
-      <input type="button" value="." onClick={() => updateCalc(".")}/>
-      <input type="button" value="=" className='btn equal' onClick={calculate}/>
-
-    </div>
-   </div>
-   </>
+    <>
+      <div className="calculator">
+        <div className="display">
+          <h2>{calc || 0}</h2>
+        </div>
+        <div className="buttons">
+          <input
+            type="button"
+            value="C"
+            className="btn"
+            onClick={clearDisplay}
+          />
+          <input
+            type="button"
+            value="DEL"
+            className="btn"
+            onClick={deleteLast}
+          />
+          <input
+            type="button"
+            value="%"
+            className="btn"
+            onClick={() => updateCalc("%")}
+          />
+          <input
+            type="button"
+            value="/"
+            className="btn"
+            onClick={() => updateCalc("/")}
+          />
+          <input type="button" value="7" onClick={() => updateCalc("7")} />
+          <input type="button" value="8" onClick={() => updateCalc("8")} />
+          <input type="button" value="9" onClick={() => updateCalc("9")} />
+          <input
+            type="button"
+            value="x"
+            className="btn"
+            onClick={() => updateCalc("*")}
+          />
+          <input type="button" value="4" onClick={() => updateCalc("4")} />
+          <input type="button" value="5" onClick={() => updateCalc("5")} />
+          <input type="button" value="6" onClick={() => updateCalc("6")} />
+          <input
+            type="button"
+            value="-"
+            className="btn"
+            onClick={() => updateCalc("-")}
+          />
+          <input type="button" value="1" onClick={() => updateCalc("1")} />
+          <input type="button" value="2" onClick={() => updateCalc("2")} />
+          <input type="button" value="3" onClick={() => updateCalc("3")} />
+          <input
+            type="button"
+            value="+"
+            className="btn"
+            onClick={() => updateCalc("+")}
+          />
+          <input type="button" value="0" onClick={() => updateCalc("0")} />
+          <input type="button" value="00" onClick={() => updateCalc("00")} />
+          <input type="button" value="." onClick={() => updateCalc(".")} />
+          <input
+            type="button"
+            value="="
+            className="btn equal"
+            onClick={calculate}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
