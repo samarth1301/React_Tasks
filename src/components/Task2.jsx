760625d115adad32e84748621eebbeca29a49125
samarth1301 <samarthsingh890.ss@gmail.com>
2023-06-18 16:54:47 +0530

    first commit

--- src/components/Task2.jsx
+++ src/components/Task2.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import "./App.css";
+import styles from "../Task2.module.css";
 
-function App() {
+function Task2() {
   const [result, setResult] = useState("");
   const [calc, setCalc] = useState("");
 
@@ -15,9 +15,9 @@ function App() {
     ) {
       return;
     }
-      setCalc(calc + value);
-      if (!ops.includes(value)) {
-        setResult(eval(calc + value).toString());
+    setCalc(calc + value);
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
     }
   };
 
@@ -40,75 +40,62 @@ function App() {
 
   return (
     <>
-      <div className="calculator">
-        <div className="display">
+      <div className={styles.calculator}>
+        <div className={styles.display}>
           <h2>{calc || 0}</h2>
         </div>
-        <div className="buttons">
-          <input
-            type="button"
-            value="C"
-            className="btn"
-            onClick={clearDisplay}
-          />
-          <input
-            type="button"
-            value="DEL"
-            className="btn"
-            onClick={deleteLast}
-          />
-          <input
-            type="button"
-            value="%"
-            className="btn"
-            onClick={() => updateCalc("%")}
-          />
-          <input
-            type="button"
-            value="/"
-            className="btn"
-            onClick={() => updateCalc("/")}
-          />
+        <div className={styles.buttons}>
           <input type="button" value="7" onClick={() => updateCalc("7")} />
           <input type="button" value="8" onClick={() => updateCalc("8")} />
           <input type="button" value="9" onClick={() => updateCalc("9")} />
           <input
             type="button"
-            value="x"
-            className="btn"
-            onClick={() => updateCalc("*")}
+            value="/"
+            className={styles.btn}
+            onClick={() => updateCalc("/")}
           />
+
+
           <input type="button" value="4" onClick={() => updateCalc("4")} />
           <input type="button" value="5" onClick={() => updateCalc("5")} />
           <input type="button" value="6" onClick={() => updateCalc("6")} />
           <input
             type="button"
-            value="-"
-            className="btn"
-            onClick={() => updateCalc("-")}
+            value="x"
+            className={styles.btn}
+            onClick={() => updateCalc("*")}
           />
+
           <input type="button" value="1" onClick={() => updateCalc("1")} />
           <input type="button" value="2" onClick={() => updateCalc("2")} />
           <input type="button" value="3" onClick={() => updateCalc("3")} />
           <input
             type="button"
-            value="+"
-            className="btn"
-            onClick={() => updateCalc("+")}
+            value="-"
+            className={styles.btn}
+            onClick={() => updateCalc("-")}
           />
+
           <input type="button" value="0" onClick={() => updateCalc("0")} />
-          <input type="button" value="00" onClick={() => updateCalc("00")} />
+
           <input type="button" value="." onClick={() => updateCalc(".")} />
           <input
             type="button"
             value="="
-            className="btn equal"
+            className={styles.btn + " " + styles.equal}
             onClick={calculate}
           />
+          <input
+            type="button"
+            value="+"
+            className={styles.btn}
+            onClick={() => updateCalc("+")}
+          />
+
         </div>
       </div>
     </>
   );
 }
 
-export default App;
+export default Task2;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Task2 from './components/Task2';
+import Index from './components/Index';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Index/>}/>
+      <Route path="/task2" element={<Task2/>} />
+    </>
+  )
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
